f34e3c36ad43f9b1e26e93ee30e334d07b861b10
junghyun-design <jhyoo@tangunsoft.com>
2026-06-07 16:06:24 +0900

    Create Search.tsx

---
+++ Search.tsx
@@ -0,0 +1,8 @@
+function Search({ placeholder = "Search anything", value = "", onChange }) {
+  return (
+    <div className="search">
+      <span className="search__icon">🔍</span>
+      <input className="search__input" placeholder={placeholder} value={value} onChange={onChange} />
+    </div>
+  );
+}
