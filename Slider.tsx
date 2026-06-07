fdf30da1056071978b3fddecafe8fab1d3177eea
junghyun-design <jhyoo@tangunsoft.com>
2026-06-07 16:05:04 +0900

    Create Slider.tsx

---
+++ Slider.tsx
@@ -0,0 +1,10 @@
+function Toggle({ checked = false, onChange }) {
+  return (
+    <label className="toggle">
+      <input type="checkbox" checked={checked} onChange={onChange} />
+      <span className="toggle__track">
+        <span className="toggle__thumb" />
+      </span>
+    </label>
+  );
+}
