5de7fa024642d46fa612ee78b226799e7ae7feef
junghyun-design <jhyoo@tangunsoft.com>
2026-06-07 16:05:24 +0900

    Create Input-field.tsx

---
+++ Input-field.tsx
@@ -0,0 +1,9 @@
+function InputField({ label = "Label", value = "", description = "Description", onChange }) {
+  return (
+    <div className="input-field">
+      <label className="input-field__label">{label}</label>
+      <input className="input-field__input" value={value} onChange={onChange} />
+      <span className="input-field__description">{description}</span>
+    </div>
+  );
+}
